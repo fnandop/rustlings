f41fa62aa308549b88b4d2c60bc50bf5003d82e2
Fernando Pinto <fnandop@gmail.com>
2023-08-01 18:35:12 +0100

    Solved exercises #1

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -3,9 +3,21 @@
 // Execute `rustlings hint variables3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
+// variables4.rs
+//
+// Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
+// hint.
+
 
 fn main() {
-    let x: i32;
+    let mut x = 3;
+    println!("Number {}", x);
+    x = 5; // don't change this line
     println!("Number {}", x);
 }
+
+/*fn main() {
+    let x: i32 = 4;
+    println!("Number {}", x);
+}*/
